a95f548dae462abcdaa7ac43935cf36cd356612c
faguilarleal <102198566+faguilarleal@users.noreply.github.com>
2025-02-25 20:29:15 -0600

    commit inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+# Client import
+from graphdatascience import GraphDataScience
+from neo4j import GraphDatabase
+
+
+AURA_CONNECTION_URI = "neo4j+s://xxxxxxxx.databases.neo4j.io"
+AURA_USERNAME = "neo4j"
+AURA_PASSWORD = "..."
+
+# Client instantiation
